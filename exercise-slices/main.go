245779c8351d4b64a0fb641b316a1cc243aec3d3
AK <ndrkrpv@gmail.com>
2025-07-18 20:30:26 +0300

    Format the exercise about slices

--- exercise-slices/main.go
+++ exercise-slices/main.go
@@ -4,18 +4,18 @@ import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
 
-	pic := make([][]uint8, dy) 
-	
-	for i:= 0; i < dy; i++ {
+	pic := make([][]uint8, dy)
+
+	for i := 0; i < dy; i++ {
 		pic[i] = make([]uint8, dx)
-		
-		for j:=0; j<dx; j++ {
+
+		for j := 0; j < dx; j++ {
 			if j%2 == 0 {
-				pic[i][j] = uint8(dx*dy)
+				pic[i][j] = uint8(dx * dy)
 			} else {
-				pic[i][j] = uint8(dx^dy)
+				pic[i][j] = uint8(dx ^ dy)
 			}
-		}	
+		}
 
 	}
 
