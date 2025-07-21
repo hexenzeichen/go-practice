1ab8a1dcacd1d123dfdc899c0d0898273aa050b1
AK <ndrkrpv@gmail.com>
2025-07-21 22:08:37 +0300

    Format exercise maps

--- exercise-maps/main.go
+++ exercise-maps/main.go
@@ -1,32 +1,30 @@
 package main
 
-import(
-	"strings"
+import (
 	"golang.org/x/tour/wc"
+	"strings"
 )
 
 func WordCount(s string) map[string]int {
 
-	 m := make(map[string]int)
-	
+	m := make(map[string]int)
+
 	sliceOfStrings := strings.Fields(s)
 
 	for _, value := range sliceOfStrings {
 
-		_, ok := m[value]		
+		_, ok := m[value]
 		if ok {
 			m[value] = m[value] + 1
 		} else {
 			m[value] = 1
 		}
 
-	}	
-	
+	}
+
 	return m
 }
 
-
-
 func main() {
-	wc.Test(WordCount)	
+	wc.Test(WordCount)
 }
