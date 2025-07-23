ab02ba02336c37496eae62844edd1ccae3db3208
AK <ndrkrpv@gmail.com>
2025-07-23 14:46:33 +0300

    Format fibonacci closure exercise

--- exercise-fibonacci-closure/main.go
+++ exercise-fibonacci-closure/main.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func fibonacci() func() int {
-	
+
 	current := 0
 	previous := 0
 	temp := 0
-	
+
 	return func() int {
-		
+
 		if current == 0 && previous == 0 {
-			current = 1	
+			current = 1
 			return 0
 		} else {
 			temp = current
@@ -22,7 +22,6 @@ func fibonacci() func() int {
 		return current
 	}
 }
-
 
 func main() {
 	f := fibonacci()
