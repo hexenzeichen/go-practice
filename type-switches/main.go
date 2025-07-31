a73b0aca3676998ac72d7645639a571d72254222
AK <ndrkrpv@gmail.com>
2025-07-31 14:05:18 +0300

    Format type switches

--- type-switches/main.go
+++ type-switches/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func do(i interface{}) {
-	
+
 	switch v := i.(type) {
 	case int:
 		fmt.Printf("Twice %v is %v\n", v, v*2)
@@ -11,7 +11,7 @@ func do(i interface{}) {
 		fmt.Printf("%q is %v bytes long\n", v, len(v))
 	default:
 		fmt.Printf("I don't know about type %T\n", v)
-		
+
 	}
 }
 
