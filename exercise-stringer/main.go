af7ada02ef7db1330d641a1d58839d03a4a767ab
AK <ndrkrpv@gmail.com>
2025-08-01 14:40:13 +0300

    Format the exercise string

--- exercise-stringer/main.go
+++ exercise-stringer/main.go
@@ -4,7 +4,6 @@ import "fmt"
 
 type IPAddr [4]byte
 
-
 func (i IPAddr) String() string {
 	return fmt.Sprintf("%v.%v.%v.%v", i[0], i[1], i[2], i[3])
 }
@@ -12,11 +11,11 @@ func (i IPAddr) String() string {
 func main() {
 
 	hosts := map[string]IPAddr{
-		"loopback": {127, 0, 0, 1},
+		"loopback":  {127, 0, 0, 1},
 		"googleDNS": {8, 8, 8, 8},
-	}	
+	}
 
 	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)	
+		fmt.Printf("%v: %v\n", name, ip)
 	}
 }
