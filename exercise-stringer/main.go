75bad8fa8ee807ac2ae614165d7f699c3910c5f0
AK <ndrkrpv@gmail.com>
2025-08-01 14:39:48 +0300

    Add the exercise stringe solution

--- exercise-stringer/main.go
+++ exercise-stringer/main.go
@@ -4,6 +4,11 @@ import "fmt"
 
 type IPAddr [4]byte
 
+
+func (i IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", i[0], i[1], i[2], i[3])
+}
+
 func main() {
 
 	hosts := map[string]IPAddr{
