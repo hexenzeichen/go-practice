b3c1a79b62fa089ffd07f070f6957757f6a89127
AK <ndrkrpv@gmail.com>
2025-08-04 08:37:29 +0300

    Add exercise readers

--- exercise-readers/main.go
+++ exercise-readers/main.go
@@ -4,6 +4,18 @@ import "golang.org/x/tour/reader"
 
 type MyReader struct{}
 
+
+func (r MyReader) Read(b []byte) (n int, err error) {
+	z := 0	
+	for i := 0; i < len(b); i++  {
+		b[i] = 'A'
+		z++
+	}
+
+	return z, nil
+}
+
+
 func main() {
 	reader.Validate(MyReader{})
 }
