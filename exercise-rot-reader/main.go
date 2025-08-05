d1a3cf429485b0dbff9adae0a97ed9cb4e81ab1d
AK <ndrkrpv@gmail.com>
2025-08-05 11:18:10 +0300

    Format exercise rot13Reader

--- exercise-rot-reader/main.go
+++ exercise-rot-reader/main.go
@@ -1,29 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"os"
 	"strings"
-	"bytes"
 )
 
 type rot13Reader struct {
 	r io.Reader
 }
 
-
 func rot13(x byte) byte {
 	input := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 	output := []byte("NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm")
-	
+
 	match := bytes.Index(input, []byte{x})
-	if (match == -1) {
+	if match == -1 {
 		return x
 	}
 	return output[match]
 }
 
-func(r13 *rot13Reader) Read(b []byte) (n int, err error) {
+func (r13 *rot13Reader) Read(b []byte) (n int, err error) {
 	n, err = r13.r.Read(b)
 	for i := 0; i <= n; i++ {
 		b[i] = rot13(b[i])
