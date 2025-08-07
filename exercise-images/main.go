2eb8e6b17d77336e1bb04b20fc9a0ff8c1332fed
AK <ndrkrpv@gmail.com>
2025-08-07 16:14:13 +0300

    Add the At() method

--- exercise-images/main.go
+++ exercise-images/main.go
@@ -18,6 +18,13 @@ func (img Image) Bounds() image.Rectangle {
 	return image.Rect(0, 0, img.w, img.h)
 }
 
+
+func (img Image) At(x, y int) color.Color {
+	v := uint8(x^y)
+	
+	return color.RGBA{v, v, 255, 255}
+}
+
 func main() {
 	m := Image{}
 	pic.ShowImage(m)
