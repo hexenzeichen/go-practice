4258782ad64fd5470f2a4281f1d4339465fbd217
AK <ndrkrpv@gmail.com>
2025-08-07 16:12:16 +0300

    Add the Bounds() method

--- exercise-images/main.go
+++ exercise-images/main.go
@@ -14,6 +14,10 @@ func (img Image) ColorModel() color.Mode {
 	return color.RGBAModel
 }
 
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, img.w, img.h)
+}
+
 func main() {
 	m := Image{}
 	pic.ShowImage(m)
