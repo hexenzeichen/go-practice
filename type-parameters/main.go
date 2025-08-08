5169e40024ea9ad936f85e5ab1108481c4edcd02
AK <ndrkrpv@gmail.com>
2025-08-08 16:37:07 +0300

    Format type parameters

--- type-parameters/main.go
+++ type-parameters/main.go
@@ -4,16 +4,15 @@ import "fmt"
 
 func Index[T comparable](s []T, x T) int {
 	for i, v := range s {
-		if (v == x) {
+		if v == x {
 			return i
-		}	
+		}
 	}
 	return -1
 }
 
-
 func main() {
-	si := []int{10, 20, 15, -10}	
+	si := []int{10, 20, 15, -10}
 	fmt.Println(Index(si, 15))
 
 	ss := []string{"foo", "bar", "baz"}
