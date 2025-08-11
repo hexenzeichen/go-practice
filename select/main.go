d3b9e8585a4d7803d2fc285cd9c89addbeeef5de
AK <ndrkrpv@gmail.com>
2025-08-11 17:53:02 +0300

    Format select

--- select/main.go
+++ select/main.go
@@ -6,15 +6,14 @@ func fibonacci(c, quit chan int) {
 	x, y := 0, 1
 	for {
 		select {
-			case c <- x:
-				x, y = y, x+y
-			case <-quit:
-				fmt.Println("quit")
-				return
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
 		}
 	}
 }
-
 
 func main() {
 	c := make(chan int)
