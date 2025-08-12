ae6b4694f73a20dd70a5371f54cd73944cac22e6
AK <ndrkrpv@gmail.com>
2025-08-12 18:38:08 +0300

    Add the tree structure

--- exercise-equivalent-binary-trees/main.go
+++ exercise-equivalent-binary-trees/main.go
@@ -2,6 +2,14 @@ package main
 
 import "golang.org/x/tour/tree"
 
+type Tree struct {
+	Left *Tree
+	Value int
+	Right *Tree
+}
+
+
+
 func Walk(t *tree.Tree, ch chan int)
 
 func Same(t1, t2 *tree.Tree) bool
